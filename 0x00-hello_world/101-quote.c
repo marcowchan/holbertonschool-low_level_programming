7de5eeb121f3373d00085bf0ac0006808fe24e92
Marco Chan <inspiredtolive0@gmail.com>
2019-06-19 12:40:35 -0700

    Use perror to pass checker

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,6 +10,6 @@ int main(void)
 {
 	const char *str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	fwrite(str, sizeof(*str), strlen(str), stderr);
+	perror(str);
 	return (1);
 }
