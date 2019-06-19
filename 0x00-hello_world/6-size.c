6dfc80b2b15dc5d65a34dbae5175540b43c1805e
Marco Chan <inspiredtolive0@gmail.com>
2019-06-19 12:26:41 -0700

    Fix punctuation

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,7 +14,7 @@ int main(void)
 	float f;
 
 	printf("Size of a char: %lu byte(s)\n", sizeof(c));
-	printf("Size of a int: %lu byte(s)\n", sizeof(i));
+	printf("Size of an int: %lu byte(s)\n", sizeof(i));
 	printf("Size of a long int: %lu byte(s)\n", sizeof(li));
 	printf("Size of a long long int: %lu byte(s)\n", sizeof(lli));
 	printf("Size of a float: %lu byte(s)\n", sizeof(f));
