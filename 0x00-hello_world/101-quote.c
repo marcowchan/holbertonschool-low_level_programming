7792d2793d77eea55a1b518c4969038f101c1503
Marco Chan <inspiredtolive0@gmail.com>
2019-06-19 15:07:04 -0700

    Use a different command to pass checker

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,4 @@
-#include <stdio.h>
-#include <string.h>
+#include <unistd.h>
 
 /**
  * main - Entry point, Print to stderr
@@ -8,9 +7,6 @@
  */
 int main(void)
 {
-	char *str  = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-
-	fwrite(str, sizeof(*str), strlen(str), stderr);
-	strerror(1);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
