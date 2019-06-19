803984e1c6a0517dbae6b980e4bb3a1e4534ffbc
Marco Chan <inspiredtolive0@gmail.com>
2019-06-19 10:36:17 -0700

    Prints without fprintf or fputs

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - Entry point, Prints without fprintf or fputs
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	const char *str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	fwrite(str, sizeof(*str), strlen(str), stdout);
+	return (0);
+}
