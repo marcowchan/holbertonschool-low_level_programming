2bbe8741854bf2c955ed56ac650fde939b82f99b
Marco Chan <inspiredtolive0@gmail.com>
2019-06-19 11:58:44 -0700

    Print to standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,15 +2,14 @@
 #include <string.h>
 
 /**
- * main - Entry point, Prints without fprintf or fputs
+ * main - Entry point, Print to stderr without fprintf or fputs
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	const char *str = "and that piece of art is useful\"" +
-	" - Dora Korpar, 2015-10-19\n";
+	const char *str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	fwrite(str, sizeof(*str), strlen(str), stdout);
+	fwrite(str, sizeof(*str), strlen(str), stderr);
 	return (0);
 }
