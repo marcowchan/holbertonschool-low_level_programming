4458f3e9b9e4b0d434c089cfd1833fca6111ed10
Marco Chan <inspiredtolive0@gmail.com>
2019-06-20 12:13:14 -0700

    Print 00 to 99 with commas and spaces

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - print 00 to 99 separated by commas and space
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int count;
+	int firstDigit;
+	int secondDigit;
+	
+	for (count = 0; count < 100; count++)
+	{
+		firstDigit = count / 10;
+		secondDigit = count % 10;
+		putchar(48 + firstDigit);
+		putchar(48 + secondDigit);
+
+		if (count != 99)
+		{
+			putchar(',');
+			putchar(' ');
+		}	
+	}
+	putchar('\n');
+	return (0);
+}
+
