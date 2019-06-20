87da53b523fde4341da85d58dfdfb3cb9c3768f2
Marco Chan <inspiredtolive0@gmail.com>
2019-06-20 12:02:04 -0700

    Print digits of base 10 with commas and spaces

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - print all digits in base 10 separated by commas
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+		if (i != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
