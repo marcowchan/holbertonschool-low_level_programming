88e779941a9f350d179001769b8661b987ede878
Marco Chan <inspiredtolive0@gmail.com>
2019-06-20 12:35:36 -0700

    Refactor code to be less than 80 chars per line

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -9,14 +9,14 @@
  */
 void printLastDigit(int n)
 {
-	int lastDigit = n % 10;
+	int digit = n % 10;
 
-	if (lastDigit > 5)
-		printf("Last digit of %i is %i and is greater than 5\n", n, lastDigit);
-	else if (lastDigit == 0)
-		printf("Last digit of %i is %i and is 0\n", n, lastDigit);
+	if (digit > 5)
+		printf("Last digit of %i is %i and is greater than 5\n", n, digit);
+	else if (digit == 0)
+		printf("Last digit of %i is %i and is 0\n", n, digit);
 	else
-		printf("Last digit of %i is %i and is less than 6 and not 0\n", n, lastDigit);
+		printf("Last digit of %i is %i and is less than 6 and not 0\n", n, digit);
 }
 
 /**
