d42a4f983758d419ff7ebbe60cb7e2f4877cb960
Marco Chan <inspiredtolive0@gmail.com>
2019-06-20 09:43:20 -0700

    Print sign of a random number

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * printSign - Print the sign of a given integer
+ * @number: integer to be tested
+*/
+void printSign(int number)
+{
+	if (number > 0)
+		printf("%i is positive\n", n);
+	else if (number == 0)
+		printf("%i is zero\n", n);
+	else
+		printf("%i is negative\n", n);
+}
+
+/**
+ * main - Generate random number
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	printSign(n);
+	return (0);
+}
