e97ce678215d466fb73cc68512db5cf8905241c5
Marco Chan <inspiredtolive0@gmail.com>
2019-06-24 10:43:32 -0700

    Add _putchar to header file

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,4 +1,4 @@
-#include "_putchar.c"
+#include "holberton.h"
 
 /**
  * main - print Holberton
--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -1,6 +1,8 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 
+void _putchar(char c);
 void print_alphabet(void);
+void print_alphabet_x10(void);
 
 #endif
