0f1618d88cbd496e3a81ee4a19774eb4a818c285
Marco Chan <inspiredtolive0@gmail.com>
2019-06-25 10:24:02 -0700

    Print n by n times table

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,38 @@
+#include "holberton.h"
+
+/**
+ * print_times_table - print nxn times table
+ * @n: number to create n by n times table
+ */
+void print_times_table(int n)
+{
+	if (n > 15 || n < 0)
+		return;
+
+	int first;
+	int second;
+	int product = 0;
+
+	for (first = 0; first <= n; first++)
+	{
+		for (second = 0; second <= n; second++)
+		{
+			product = first * second;
+			if (second > 0)
+			{
+				_putchar(',');
+				if (product < 100)
+					_putchar(' ');
+				if (product < 10)
+					_putchar(' ');
+				_putchar(' ');
+			}
+			if (product >= 100)
+				_putchar('0' + product / 100);
+			if (product >= 10)
+				_putchar('0' + product / 10 % 10);
+			_putchar('0' + product % 10);
+		}
+		_putchar('\n');
+	}
+}
