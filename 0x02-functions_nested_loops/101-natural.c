3bac45f877089659bb46e0795c3f298241c80acf
Marco Chan <inspiredtolive0@gmail.com>
2019-06-25 10:39:00 -0700

    Print sum of nums divisible by 3 or 5 from 0-1024

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - sum of all nums divisible by 3 or 5 from 0-1024
+ * Return: Success 0
+ */
+int main(void)
+{
+	int n;
+	int sum = 0;
+
+	for (n = 0; n < 1024; n++)
+	{
+		if (n % 3 == 0)
+			sum += n;
+		else if (n % 5 == 0)
+			sum += n;
+	}
+	printf("%d\n", sum);
+}
