55668db2dfe783942d35c1199facae93cc575008
Marco Chan <inspiredtolive0@gmail.com>
2019-06-27 10:04:38 -0700

    Compute product of two numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,10 @@
+/**
+ * mul - compute the product of two numbers
+ * Return: product of two numbers
+ * @a: number to multiply with b
+ * @b: number to multiply with a
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
