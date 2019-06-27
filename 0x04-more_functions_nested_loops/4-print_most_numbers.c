d1a7c5de807bf0ef3976059a708d123ee5aa9884
Marco Chan <inspiredtolive0@gmail.com>
2019-06-27 12:41:52 -0700

    Rename function

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,9 +1,9 @@
 #include "holberton.h"
 
 /**
- * print_numbers - print numbers from 0 - 9, except 2 and 4
+ * print_most_numbers - print numbers from 0 - 9, except 2 and 4
  */
-void print_numbers(void)
+void print_most_numbers(void)
 {
 	char i;
 
