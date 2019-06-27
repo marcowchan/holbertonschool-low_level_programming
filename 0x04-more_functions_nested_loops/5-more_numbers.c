c22718aa14d8b24706171b18149aeef0a091224f
Marco Chan <inspiredtolive0@gmail.com>
2019-06-27 10:33:31 -0700

    Print 0 - 14 ten times

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * more_numbers - print 0 - 14 ten times
+ */
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			if (j > 10)
+				_putchar('0' + j / 10);
+			_putchar('0' + j % 10);
+		}
+		_putchar('\n');
+	}
+}
