c430ffab5f0dcf4cadc6f0b10dae9ba890f6ba88
Marco Chan <inspiredtolive0@gmail.com>
2019-06-27 22:53:42 -0700

    Handles edge case of INT_MIN

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,12 +6,17 @@
  */
 void print_number(int n)
 {
-	if (n % 10 < 0)
+	int lastDigit;
+
+	if (n < 0)
 	{
 		_putchar('-');
-		n = -n;
+		lastDigit = n % 10 * -1;
+		n = n / -10;
+		print_number(n);
+		_putchar('0' + lastDigit);
 	}
-	if (n < 10)
+	else if (n < 10)
 		_putchar('0' + n % 10);
 	else
 	{
