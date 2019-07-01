da01922d04f85733efa7a500d9110b154f93ed11
Marco Chan <inspiredtolive0@gmail.com>
2019-07-01 11:15:14 -0700

    Print every other character of string

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * puts2 - print every second character of string
+ * @str: string to print characters from
+ */
+void puts2(char *str)
+{
+	if (*str == 0)
+		return;
+
+	do {
+		_putchar(*str);
+		str += 2;
+	} while (*str);
+	_putchar('\n');
+}
--- 0x05-pointers_arrays_strings/holberton.h
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -8,5 +8,6 @@ void _putchar(char c);
 void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
+void puts2(char *str);
 
 #endif
