2d4ac883df87cc46975fd33263fbbbbf95a6f9f3
Marco Chan <inspiredtolive0@gmail.com>
2019-07-05 15:29:21 -0700

    Transform string to uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,17 @@
+/**
+ * string_toupper - change all lowercase to uppercase
+ * @s: string to transform
+ * Return: pointer to transformed string
+ */
+char *string_toupper(char *s)
+{
+	char *r = s;
+
+	while (*s)
+	{
+		if (*s >= 'a' && *s <= 'z')
+			*s -= 32;
+		s++;
+	}
+	return (r);
+}
