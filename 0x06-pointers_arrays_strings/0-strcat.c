1ce80d365ab2f82c9ab8f23a6abace91b7db9b55
Marco Chan <inspiredtolive0@gmail.com>
2019-07-05 11:44:26 -0700

    Concat two strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,22 @@
+/**
+ * _strcat - concatenates two strings
+ * @dest: string to concate with src
+ * @src: string to concate to dest
+ * Return: pointer to dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	char *p = dest;
+
+	while (*dest++)
+		;
+	dest--;
+	while (*src)
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+	return (p);
+}
