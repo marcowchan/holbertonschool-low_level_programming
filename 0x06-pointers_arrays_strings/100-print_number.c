2c0672c95ec6d7a4f8ed2186573a11ee500a664e
Marco Chan <inspiredtolive0@gmail.com>
2019-07-08 06:36:27 +0000

    Fix output for INT_MIN

--- 0x06-pointers_arrays_strings/100-print_number.c
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -10,15 +10,12 @@ void print_number(int n)
 		_putchar('-');
 		if (n < -9)
 			print_number(n / -10);
-		_putchar(-n % 10 + '0');
-	}
-	else if (n > 9)
-	{
-		print_number(n / 10);
-		_putchar(n % 10 + '0');
+		_putchar('0' - n % 10);
 	}
 	else
 	{
+		if (n > 9)
+			print_number(n / 10);
 		_putchar(n % 10 + '0');
 	}
 }
