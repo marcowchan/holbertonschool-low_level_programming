edf53fa668fe0cd3cb7cdd2768e0798cc6fbbe25
Marco Chan <inspiredtolive0@gmail.com>
2019-07-16 21:43:01 -0700

    Treat NULL input as empty strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,6 +11,10 @@ char *str_concat(char *s1, char *s2)
 	int len1, len2, i, j;
 	char *p;
 
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 	for (len1 = 0; s1[len1]; len1++)
 		;
 	for (len2 = 0; s2[len2]; len2++)
