511799efc1dc5564a3203e01e65320a750db5015
Marco Chan <inspiredtolive0@gmail.com>
2019-07-18 21:26:10 -0700

    Conform to betty style

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -3,10 +3,12 @@
 /**
  * malloc_checked - allocate memory
  * @b: number of bytes to allocate
+ * Return: pointer to allocated memory
  */
 void *malloc_checked(unsigned int b)
 {
 	void *p = malloc(b);
+
 	if (p == NULL)
 		exit(98);
 	return (p);
