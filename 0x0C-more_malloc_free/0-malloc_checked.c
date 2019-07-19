a2e066bce6a63e85dfaf378f0d36f0dd95782ac5
Marco Chan <inspiredtolive0@gmail.com>
2019-07-19 01:49:37 +0000

    Allocate memory

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+
+/**
+ * malloc_checked - allocate memory
+ * @b: number of bytes to allocate
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *p = malloc(b);
+	if (p == NULL)
+		exit(98);
+	return (p);
+}
