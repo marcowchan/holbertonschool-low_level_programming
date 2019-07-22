50ddb082d1c2e4442ac235b4309df51d8c438f1a
Marco <inspiredtolive0@gmail.com>
2019-07-22 22:06:18 +0000

    Initialize members of dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,6 @@
 #ifndef DOG_H
 #define DOG_H
+
 /**
  * struct dog - simple dog struct
  * @name: name of dog
@@ -12,4 +13,6 @@ struct dog
 	float age;
 	char *owner;
 };
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
 #endif
