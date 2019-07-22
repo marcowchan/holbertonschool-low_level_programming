3cb601a23632c0a05c3c6e67bc85d6218a07d38a
Marco <inspiredtolive0@gmail.com>
2019-07-22 21:29:38 +0000

    Define dog struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - simple dog struct
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
