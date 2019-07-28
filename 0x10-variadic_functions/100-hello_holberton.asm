b6a1041a51cfabb9eb4ef58d30139e309fdee408
Marco Chan <inspiredtolive0@gmail.com>
2019-07-28 00:01:13 -0700

    Properly exit program

--- 0x10-variadic_functions/100-hello_holberton.asm
+++ 0x10-variadic_functions/100-hello_holberton.asm
@@ -1,13 +1,17 @@
 SECTION .data
 msg     db      'Hello, Holberton', 0Ah
-
+ 
 SECTION .text
-global  _start
-
-_start:
-
+global  main
+ 
+main:
+ 
     mov     edx, 17
     mov     ecx, msg
     mov     ebx, 1
     mov     eax, 4
+    int     80h
+ 
+    mov     ebx, 0
+    mov     eax, 1
     int     80h
