81459ccb44717169fc8a8b0d56de47a0c1695f45
Marco Chan <inspiredtolive0@gmail.com>
2019-08-02 06:43:00 +0000

    Removes interrupts

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -2,14 +2,14 @@ section .data
 	msg	db "Hello, Holberton", 10, 0
 
 section .text
-	extern printf
-	global main
+	extern	printf
+	global	main
 
 main:
-	mov  edi, msg
-	mov  eax, 0
-	call printf
+	mov	edi, msg
+	mov	eax, 0
+	call	printf
 
-	mov  ebx, 0
-	mov  eax, 1
-	int  0x80
+	mov	ebx, 0
+	mov	eax, 1
+	ret
