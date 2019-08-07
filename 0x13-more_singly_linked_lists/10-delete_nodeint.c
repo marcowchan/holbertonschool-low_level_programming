5457f702fc54f31a5d6ac3c24126d33dfd1ca9b8
Marco Chan <inspiredtolive0@gmail.com>
2019-08-07 23:12:33 +0000

    Remove subtraction of unsigned number

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -4,35 +4,36 @@
 /**
  * delete_nodeint_at_index - deletes a node at a given index
  * @head: head of the linked list
- * @index: index of the node to be deleted
+ * @index: index of the prev to be deleted
  * Return: Success(1), Error(-1)
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-	listint_t *node = *head;
-	listint_t *delete;
+	listint_t *prev = *head;
+	listint_t *delete = NULL;
 	unsigned int i;
 
-	if (!head || !(*head))
+	if (!(*head))
 		return (-1);
 
-	for (i = 0; i < index - 1; i++)
+	for (i = 1; i < index; i++)
 	{
-		if (node->next)
-			node = node->next;
+		if (prev->next)
+			prev = prev->next;
 		else
 			return (-1);
 	}
 
 	if (index == 0)
 	{
-		delete = node;
-		*head = node->next;
+		delete = prev;
+		*head = (*head)->next;
 	}
 	else
-		delete = node->next;
-
-	node->next = delete->next;
+	{
+		delete = prev->next;
+		prev->next = delete->next;
+	}
 	free(delete);
 	return (1);
 }
