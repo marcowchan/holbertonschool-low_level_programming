495c112364ef3c6098b985246497e9a655e69d4d
Marco Chan <inspiredtolive0@gmail.com>
2019-08-08 19:01:56 +0000

    Removes division

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -9,7 +9,7 @@ void print_binary(unsigned long int n)
 	unsigned long int bits = sizeof(long int) * 8, lead_zero = 1;
 	unsigned long int bit = 1;
 
-	for (bit = bit << (bits - 1); bit; bit /= 2)
+	for (bit = bit << (bits - 1); bit; bit = bit >> 1)
 	{
 		if ((n & bit) && lead_zero)
 			lead_zero = 0;
