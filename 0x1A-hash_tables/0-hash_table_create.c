d1d1e7c695ecbcaeda78c7e361e2a50b652b59d7
Marco Chan <inspiredtolive0@gmail.com>
2019-10-11 19:26:10 -0700

    Frees the table if array malloc fails

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,11 +10,16 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *table;
 
 	if (size <= 0)
-	return (NULL);
+		return (NULL);
 	table = malloc(sizeof(*table));
 	if (table == NULL)
-	return (NULL);
+		return (NULL);
 	table->size = size;
 	table->array = malloc(sizeof(*(table->array)) * size);
+	if (table->array == NULL)
+	{
+		free(table);
+		return(NULL);
+	}
 	return (table);
 }
