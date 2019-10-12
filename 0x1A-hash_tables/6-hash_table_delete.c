0583ced79ffe6c2dc6d622ca079c50b757446613
Marco Chan <inspiredtolive0@gmail.com>
2019-10-11 21:04:07 -0700

    Frees a hash table

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,28 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - deallocates a hash table
+ * @ht: hash table to deallocate
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *tuple = NULL, *delete = NULL;
+
+	if (ht == NULL)
+		return;
+	for (i = 0; i < ht->size; i++)
+	{
+		tuple = ht->array[i];
+		while (tuple != NULL)
+		{
+			delete = tuple;
+			tuple = tuple->next;
+			free(delete->key);
+			free(delete->value);
+			free(delete);
+		}
+	}
+	free(ht->array);
+	free(ht);
+}
