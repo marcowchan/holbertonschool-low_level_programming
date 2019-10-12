c1c4d5543125bd4fb00063431f91c76fdb9921a3
Marco Chan <inspiredtolive0@gmail.com>
2019-10-11 17:33:13 -0700

    Adds djb2 hashing function

---
+++ 0x1A-hash_tables/1-djb2.c
@@ -0,0 +1,15 @@
+/**
+ * hash_djb2 - djb2 hashing function
+ * @str: string to hash
+ * Return: hash
+ */
+unsigned long int hash_djb2(const unsigned char *str)
+{
+	unsigned long hash = 5381;
+	int c;
+
+	while ((c = *str++))
+		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+
+	return (hash);
+}
