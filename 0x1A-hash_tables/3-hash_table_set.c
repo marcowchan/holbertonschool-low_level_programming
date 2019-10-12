f00a7c07c73d491e021d0b36afda49d027cfc0f1
Marco Chan <inspiredtolive0@gmail.com>
2019-10-11 20:28:08 -0700

    Checks if strdup successfully allocated memeory

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,5 +1,4 @@
 #include "hash_tables.h"
-#include <string.h>
 
 /**
  * hash_table_set - adds a key-value pair to a hash table
@@ -12,16 +11,23 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
 	hash_node_t *tuple;
+	char *new_value = NULL;
 
-	if (key == NULL || strcmp(key, "") == 0)
+	if (ht == NULL || key == NULL || strcmp(key, "") == 0)
 		return (0);
 	index = key_index((const unsigned char *)key, ht->size);
 	for (tuple = ht->array[index]; tuple != NULL; tuple = tuple->next)
 	{
 		if (strcmp(tuple->key, key) == 0)
 		{
+			if (value != NULL)
+			{
+				new_value = strdup(value);
+				if (new_value == NULL)
+					return (0);
+			}
 			free(tuple->value);
-			tuple->value = strdup(value);
+			tuple->value = new_value;
 			return (1);
 		}
 	}
@@ -30,6 +36,13 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 	tuple->key = strdup(key);
 	tuple->value = strdup(value);
+	if (tuple->key == NULL || tuple->value == NULL)
+	{
+		free(tuple->key);
+		free(tuple->value);
+		free(tuple);
+		return (0);
+	}
 	tuple->next = ht->array[index];
 	ht->array[index] = tuple;
 	return (1);
