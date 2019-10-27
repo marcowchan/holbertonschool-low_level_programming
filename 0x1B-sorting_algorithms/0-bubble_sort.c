d3b27454496f01791d1e72567a70eb5dd0a02a9d
Marco Chan <inspiredtolive0@gmail.com>
2019-10-26 18:33:43 -0700

    Bubble sorts an array of ints

---
+++ 0x1B-sorting_algorithms/0-bubble_sort.c
@@ -0,0 +1,32 @@
+#include "sort.h"
+#include <stdio.h>
+
+/**
+ * bubble_sort - sorts an array using the bubble sort algorithm
+ * @array: the array to sort
+ * @size: the size of the array
+ */
+void bubble_sort(int *array, size_t size)
+{
+	bool isUnsorted = true;
+	size_t i = 0;
+	int tmp;
+
+	if (array == NULL || size < 2)
+		return;
+	while (isUnsorted)
+	{
+		isUnsorted = false;
+		for (i = 0; i < size - 1; i++)
+		{
+			if (array[i] > array[i + 1])
+			{
+				isUnsorted = true;
+				tmp = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = tmp;
+				print_array(array, size);
+			}
+		}
+	}
+}
