11ebf5221ac175fb38dd42aeca64fccdd5b4061b
Marco Chan <inspiredtolive0@gmail.com>
2019-12-04 14:41:16 -0800

    Fixes function names

--- 0x1D-binary_trees/11-binary_tree_size.c
+++ 0x1D-binary_trees/11-binary_tree_size.c
@@ -11,7 +11,7 @@ size_t binary_tree_size(const binary_tree_t *tree)
 
 	if (!tree)
 		return (0);
-	size = binary_tree_counter(tree->left);
-	size += binary_tree_counter(tree->right);
+	size = binary_tree_size(tree->left);
+	size += binary_tree_size(tree->right);
 	return (size + 1);
 }
