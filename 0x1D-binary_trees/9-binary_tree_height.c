efb729b21ccebe1e19de00708ddaf1d358bc89a8
Marco Chan <inspiredtolive0@gmail.com>
2019-12-05 13:00:29 -0800

    Handles NULL root

--- 0x1D-binary_trees/9-binary_tree_height.c
+++ 0x1D-binary_trees/9-binary_tree_height.c
@@ -25,9 +25,11 @@ size_t recurse(const binary_tree_t *tree)
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t depth;
+	size_t depth = 0;
 
-	for (depth = 0; tree && tree->parent; tree = tree->parent, depth++)
+	if (!tree)
+		return (depth);
+	for (; tree && tree->parent; tree = tree->parent, depth++)
 		;
 	return (recurse(tree) - 1 - depth);
 }
