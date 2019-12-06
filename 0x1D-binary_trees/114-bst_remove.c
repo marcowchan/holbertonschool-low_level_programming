2226849dfe507f83a8106481a900ada125e6db2f
Marco Chan <inspiredtolive0@gmail.com>
2019-12-06 15:58:59 -0800

    Fixes parent pointer

--- 0x1D-binary_trees/114-bst_remove.c
+++ 0x1D-binary_trees/114-bst_remove.c
@@ -29,9 +29,17 @@ bst_t *bst_remove(bst_t *root, int value)
 	if (root == NULL)
 		return (root);
 	if (value < root->n)
+	{
 		root->left = bst_remove(root->left, value);
+		if (root->left)
+			root->left->parent = root;
+	}
 	else if (value > root->n)
+	{
 		root->right = bst_remove(root->right, value);
+		if (root->right)
+			root->right->parent = root;
+	}
 	else
 	{
 		if (root->left == NULL)
