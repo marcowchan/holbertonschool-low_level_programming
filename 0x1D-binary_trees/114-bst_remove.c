71c1647056dc24d2f8118b2589ac8cec69738259
Marco Chan <inspiredtolive0@gmail.com>
2019-12-06 14:45:30 -0800

    Removes a node from a Binary Search Tree

---
+++ 0x1D-binary_trees/114-bst_remove.c
@@ -0,0 +1,54 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "binary_trees.h"
+
+/**
+ * minValueNode - Gets the smallest node in the stree.
+ * @root: The pointer to the root node.
+ * @value: The value to remove in the tree.
+ * Return: The pointer to the new root node of the tree.
+ */
+bst_t *minValueNode(bst_t *node)
+{
+    bst_t *current = node;
+    while (current && current->left != NULL)
+        current = current->left;
+    return current;
+}
+
+/**
+ * bst_remove - Removes a node from a Binary Search Tree.
+ * @root: The pointer to the root node.
+ * @value: The value to remove in the tree.
+ * Return: The pointer to the new root node of the tree.
+ */
+bst_t *bst_remove(bst_t *root, int value)
+{
+	bst_t *temp;
+
+	if (root == NULL)
+		return (root);
+	if (value < root->n)
+		root->left = bst_remove(root->left, value);
+	else if (value > root->n)
+		root->right = bst_remove(root->right, value);
+	else
+	{
+		if (root->left == NULL)
+		{
+			temp = root->right;
+			free(root);
+			return (temp);
+		}
+		else if (root->right == NULL)
+		{
+			temp = root->left;
+			free(root);
+			return (temp);
+		}
+		temp = minValueNode(root->right);
+		root->n = temp->n;
+		root->right = bst_remove(root->right, temp->n);
+	}
+	return (root);
+}
